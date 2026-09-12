1f421c32c92576b5e1a05cf322d1872bfeb6d39d
amit391 <amitsit2k11@gmail.com>
2026-09-12 17:10:05 +0530

    first commit

---
+++ render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Update pip and install python requirements
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Install system dependencies needed for OpenCV on Render's Ubuntu environment
+apt-get update && apt-get install -y libgl1-mesa-glx libglib2.0-0
